ac4ab65cf0fe360a9fa3ad1f23863b3ed0eaaf04
Azad Yekta <azad@yekta.de>
2022-05-09 12:10:15 +0200

    completed password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw-generator';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+
+    console.log("Length is : " + this.length);
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick(){
+    const allowedLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const allowedNumbers = '0123456789';
+    const allowedSymbols = '~`! @#$%^&*()_-+={[}]|\:;"<,>.?/';
+
+    let allowedPassword = ''
+
+    if(this.includeLetters){
+      allowedPassword += allowedLetters;
+    }
+
+    if(this.includeNumbers){
+      allowedPassword += allowedNumbers;
+    }
+
+    if(this.includeSymbols){
+      allowedPassword += allowedSymbols;
+  }
+
+  let generatedPassword = '';
+
+  console.log(allowedPassword);
+
+  for(let i = 0; i < this.length; i++){
+    const index = Math.floor(Math.random() * allowedPassword.length);
+    generatedPassword += allowedPassword[index];
+    console.log(allowedPassword.length);
+    console.log(allowedPassword[index]);
+    console.log(generatedPassword);
+  }
+
+  
+    this.password = generatedPassword;
+    console.log(this.password);
+  }
+
+
 }
